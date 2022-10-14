2456cac3004fe6e61930e36dde71c022c7f910a1
Nghi1 <nghivu201@gmail.com>
2022-10-14 19:55:43 +0700

    add score

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ let gameOver=document.getElementById("over")
 let game= document.getElementById("game")
 let batman=document.getElementById("batman")
 let joker=document.getElementById("joker")
+let score=document.getElementById("score")
+let point=0
+let status=false
 game.classList.add("begin")
 function jump(){
     cactus.classList.add("run")
@@ -14,16 +17,26 @@ function jump(){
     }, 1000)
 }
 setInterval(()=>{
-    dinoTop=parseInt(window.getComputedStyle(dino).top)
+    if(status)
+    score.innerHTML=`Score: 0${point+=1}`
+}, 1000)
+setInterval(()=>{
+    if(status){
+        dinoTop=parseInt(window.getComputedStyle(dino).top)
     cactusLeft=parseInt(window.getComputedStyle(cactus).left)
     if(cactusLeft>0 && cactusLeft<50 && dinoTop>=230){
         cactus.classList.remove("run")
         gameOver.classList.add("block")
+        status=false
+        point=0
+    }
     }
 }, 10)
 document.addEventListener("keydown", function(event){
     game.classList.remove("begin")
-    jump()})
+    jump()
+    status=true
+    })
 function batmanClick(){
     dino.classList.remove("joker")
     dino.classList.add("batman")
